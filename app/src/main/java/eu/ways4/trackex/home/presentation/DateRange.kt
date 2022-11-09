c6ff5d02739079809175c25cf1381ab6f0c76843
smartDevel <57102149+smartDevel@users.noreply.github.com>
2022-11-09 14:21:33 +0100

    20221109_addedExpenseFilterDates_MonthBack

--- app/src/main/java/eu/ways4/trackex/home/presentation/DateRange.kt
+++ app/src/main/java/eu/ways4/trackex/home/presentation/DateRange.kt
@@ -31,7 +31,65 @@ enum class DateRange {
 
     ALL_TIME {
         override fun contains(date: LocalDate) = true
+    },
+
+    LAST_MONTH {
+        override fun contains(date: LocalDate): Boolean {
+            val now = LocalDate.now().minusMonths(1)
+            val dayBeforeFirstDayOfMonth = now.firstDayOfMonth().yesterday()
+            val dayAfterLastDayOfMonth = now.lastDayOfMonth().tomorrow()
+            return date.isAfter(dayBeforeFirstDayOfMonth) && date.isBefore(dayAfterLastDayOfMonth)
+        }
+    },
+
+    MONTH_MINUS02 {
+        override fun contains(date: LocalDate): Boolean {
+            val now = LocalDate.now().minusMonths(2)
+            val dayBeforeFirstDayOfMonth = now.firstDayOfMonth().yesterday()
+            val dayAfterLastDayOfMonth = now.lastDayOfMonth().tomorrow()
+            return date.isAfter(dayBeforeFirstDayOfMonth) && date.isBefore(dayAfterLastDayOfMonth)
+        }
+    },
+    MONTH_MINUS03 {
+        override fun contains(date: LocalDate): Boolean {
+            val now = LocalDate.now().minusMonths(3)
+            val dayBeforeFirstDayOfMonth = now.firstDayOfMonth().yesterday()
+            val dayAfterLastDayOfMonth = now.lastDayOfMonth().tomorrow()
+            return date.isAfter(dayBeforeFirstDayOfMonth) && date.isBefore(dayAfterLastDayOfMonth)
+        }
+    },
+    MONTH_MINUS12 {
+        override fun contains(date: LocalDate): Boolean {
+            val now = LocalDate.now().minusMonths(12)
+            val dayBeforeFirstDayOfMonth = now.firstDayOfMonth().yesterday()
+            val dayAfterLastDayOfMonth = now.lastDayOfMonth().tomorrow()
+            return date.isAfter(dayBeforeFirstDayOfMonth) && date.isBefore(dayAfterLastDayOfMonth)
+        }
+    },
+    OCTOBER {
+        override fun contains(date: LocalDate): Boolean {
+            val now = LocalDate.now()
+            val yr = now.year.toString()
+            val first = LocalDate.parse("$yr-10-01")
+            val last = LocalDate.parse("$yr-10-31")
+            val dayBeforeFirstDayOfMonth = first.firstDayOfMonth().yesterday()
+            val dayAfterLastDayOfMonth = last.lastDayOfMonth().tomorrow()
+            return date.isAfter(dayBeforeFirstDayOfMonth) && date.isBefore(dayAfterLastDayOfMonth)
+        }
+    },
+
+    NOVEMBER {
+        override fun contains(date: LocalDate): Boolean {
+            val now = LocalDate.now()
+            val yr = now.year.toString()
+            val first = LocalDate.parse("$yr-11-01")
+            val last = LocalDate.parse("$yr-11-30")
+            val dayBeforeFirstDayOfMonth = first.firstDayOfMonth().yesterday()
+            val dayAfterLastDayOfMonth = last.lastDayOfMonth().tomorrow()
+            return date.isAfter(dayBeforeFirstDayOfMonth) && date.isBefore(dayAfterLastDayOfMonth)
+        }
     };
+
 
     abstract fun contains(date: LocalDate): Boolean
 }
--- app/src/main/java/eu/ways4/trackex/home/presentation/SummaryItemModel.kt
+++ app/src/main/java/eu/ways4/trackex/home/presentation/SummaryItemModel.kt
@@ -31,7 +31,13 @@ class SummaryItemModel(
             TODAY -> context.getString(R.string.today)
             THIS_WEEK -> context.getString(R.string.this_week)
             THIS_MONTH -> context.getString(R.string.this_month)
+            LAST_MONTH -> context.getString(R.string.last_month)
+            MONTH_MINUS02 -> context.getString(R.string.month_minus02)
+            MONTH_MINUS03 -> context.getString(R.string.month_minus03)
+            MONTH_MINUS12 -> context.getString(R.string.month_minus12)
             ALL_TIME -> context.getString(R.string.all_time)
+            OCTOBER -> context.getString(R.string.october)
+            NOVEMBER -> context.getString(R.string.november)
         }
     }
 
@@ -40,6 +46,18 @@ class SummaryItemModel(
     fun onThisWeekClick() = dateRangeChange?.invoke(DateRange.THIS_WEEK)
 
     fun onThisMonthClick() = dateRangeChange?.invoke(DateRange.THIS_MONTH)
+
+    fun onLastMonthClick() = dateRangeChange?.invoke(DateRange.LAST_MONTH)
+
+    fun onMonthMinus02Click() = dateRangeChange?.invoke(DateRange.MONTH_MINUS02)
+
+    fun onMonthMinus03Click() = dateRangeChange?.invoke(DateRange.MONTH_MINUS03)
+
+    fun onMonthMinus12Click() = dateRangeChange?.invoke(DateRange.MONTH_MINUS12)
+
+    fun onOctoberClick() = dateRangeChange?.invoke(DateRange.OCTOBER)
+
+    fun onNovemberClick() = dateRangeChange?.invoke(DateRange.NOVEMBER)
 
     fun onAllTimeClick() = dateRangeChange?.invoke(DateRange.ALL_TIME)
 }
